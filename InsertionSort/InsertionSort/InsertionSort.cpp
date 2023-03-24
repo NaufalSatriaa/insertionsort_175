d4b3ed88cb5c50fa4636c68c19c77a981b1f8973
NaufalSatriaa <satriana0017@gmail.com>
2023-03-24 11:06:15 +0700

    membuat looping dengan j dimulai dari 0 hingga n-1 dan di display ke layar

--- InsertionSort/InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort/InsertionSort.cpp
@@ -65,4 +65,9 @@ void display() {
 	cout << "=================================" << endl;
 	cout << "element array yang telah tersusun" << endl;
 	cout << "=================================" << endl;
+
+	for (int j = 0; j < n; j++ {
+		cout << arr[j] << endl;
+	}
+	cout << endl;
 }
