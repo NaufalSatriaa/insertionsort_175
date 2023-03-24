8e8ab69fb0ff349cdcae94aa8d6d7423524843e8
NaufalSatriaa <satriana0017@gmail.com>
2023-03-24 10:29:56 +0700

    membuat keterangan pada koding

--- InsertionSort/InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort/InsertionSort.cpp
@@ -7,18 +7,19 @@ int n; //membuat variable inputan n
 void input() { //procedure input
 	while (true)
 	{
-		cout << "masukkan jumlah data pada array : ";
-		cin >> n;
-		if (n <= 20) {
+		cout << "masukkan jumlah data pada array : "; //membuat inputan jumlah element array
+ 		cin >> n; //memanggil variabel inputan n
+
+		if (n <= 20) { //membuat kondisi n tidak lebih dari 20
 			break;
 		}
 		else {
-			cout << "\narray yang anda masukkan maksimal 20 elemen.\n";
+			cout << "\narray yang anda masukkan maksimal 20 elemen.\n"; //menampilkan pesan jika data lebih dari 20
 
 		}
 	}
-	cout << endl;
-	cout << "=====================" << endl;
+	cout << endl; //membuat jarak per baris program
+	cout << "=====================" << endl; //membuat tampilan susunan data element array
 	cout << "masukkan elemen array" << endl;
 	cout << "=====================" << endl;
 }
